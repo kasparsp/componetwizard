56427f5dca25b9794aa379756f262baedc3b678d
varezers <k@varezers.org>
2017-06-14 00:15:23 +0300

    0.1.0

---
+++ spec/otmprocomponet-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import OtmprocomponetView from '../lib/otmprocomponet-view';
+
+describe('OtmprocomponetView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
